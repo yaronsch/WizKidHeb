07a2b552eeac4b8f1c4f793dceabfa3f3f70ebc8
Yaron Schwimmer <yaronschwimmer@gmail.com>
2016-06-12 11:13:36 +0300

    hud buttons

--- app/components/hud/hud.ts
+++ app/components/hud/hud.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation, Input} from "@angular/core";
+import {Component, ViewEncapsulation, Input, Output, EventEmitter} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {PlayerDataService} from "../../services/player-data-service";
 
@@ -10,21 +10,34 @@ import {PlayerDataService} from "../../services/player-data-service";
                 <span class="button-inner"><img src="build/images/buttons/back.png">חזרה </span>
                 <ion-button-effect></ion-button-effect>
             </button>
-            <ion-select *ngIf="playerData" [ngModel]="playerData.level" (ngModelChange)="onLevelSelected($event)">
-                <ion-option value="1">קל</ion-option>
-                <ion-option value="2">בינוני</ion-option>
-                <ion-option value="3">קשה</ion-option>                
-             </ion-select>
+            <button *ngIf="showNextEx" (click)="nextEx()">התרגיל הבא</button>
+            <div *ngIf="playerData" class="level-select">
+                <ion-item>         
+                    <ion-label></ion-label>
+                    <ion-select [ngModel]="playerData.level" (ngModelChange)="onLevelSelected($event)"
+                    okText="אישור" cancelText="ביטול" [alertOptions]="{title:'רמת קושי'}">
+                        <ion-option value="1">קל</ion-option>
+                        <ion-option value="2">בינוני</ion-option>
+                        <ion-option value="3">קשה</ion-option>                
+                     </ion-select>                     
+                </ion-item>               
+            </div>
         </div>
     `,
     encapsulation: ViewEncapsulation.None
 })
 export class Hud {
     @Input() playerData: PlayerDataService;
+    @Input() showNextEx: boolean;
+    @Output('nextEx') nextExClicked: EventEmitter<any> = new EventEmitter();
     constructor(private nav: NavController) {}
 
     goBack() {
         this.nav.pop();
+    }
+    
+    nextEx() {
+        this.nextExClicked.emit('clicked');
     }
 
     onLevelSelected(level: number) {
