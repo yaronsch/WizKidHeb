88e88a50c8074cf2bbee20dff6569aa61ac27779
Yaron Schwimmer <yaronschwimmer@gmail.com>
2016-07-10 22:53:32 +0300

    flags work

--- app/pages/general/flags/flags.ts
+++ app/pages/general/flags/flags.ts
@@ -17,6 +17,8 @@ const GAME = 1;
 export class FlagsPage {
     gameData: any;
     country: any;
+    suggestions: any[];
+    availableCountries: any[] = [];
 
     constructor(private staticDataService: StaticDataService) {
         this.gameData = staticDataService.data.menu[CATEGORY].games[GAME];        
@@ -24,7 +26,38 @@ export class FlagsPage {
     }
 
     nextExercise() {
-        let index = Math.floor(Math.random() * this.gameData.data.flags.length);
-        this.country = this.gameData.data.flags[index];
+        if (this.availableCountries.length === 0) {
+            this.availableCountries = this.gameData.data.flags.slice(0);
+        }
+        let index = Math.floor(Math.random() * this.availableCountries.length);
+        this.country = this.availableCountries.splice(index, 1)[0];
+        this.generateSuggestions();
+    }
+
+    generateSuggestions() {
+        let suggestions = [this.country];
+        while (suggestions.length < NUM_SUGGESTIONS) {
+            let index = Math.floor(Math.random() * this.gameData.data.flags.length);
+            let suggestion: any = this.gameData.data.flags[index];
+            while (suggestions.find(item => item.name === suggestion.name)) {
+                index = Math.floor(Math.random() * this.gameData.data.flags.length);
+                suggestion = this.gameData.data.flags[index];
+            }
+            suggestions.push(Object.assign({}, suggestion));
+        }
+        this.suggestions = suggestions;
+    }
+
+    itemClicked(suggestion) {
+        if (suggestion.name === this.country.name) {
+            //TODO success
+            setTimeout(() => {
+                this.nextExercise();
+            }, 1000)
+        }
+        else {
+            //TODO fail
+            suggestion.used = true;
+        }
     }
 }
